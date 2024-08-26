abb3087624ae3a8e12ca17e1dfe35dd629847d32
Pavan Gupta <Pavan.Gupta@blueyonder.com>
2024-08-26 08:14:48 +0530

    Adding other springboot files

---
+++ src/main/java/com/intu/product/IntuitiveProductApplication.java
@@ -0,0 +1,13 @@
+package com.intu.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitiveProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntuitiveProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intu/product/IntuitiveProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intu.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuitiveProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
